fce1547e50da1f60e784accb24f943a87e3b4cf7
cj20010613 <68772369+cj20010613@users.noreply.github.com>
2021-05-14 00:32:23 +0800

    3
    3

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -2,6 +2,7 @@
   <q-page class="flex flex-center row">
     <div class="col-12" style="height: 20vh;"></div>
     <div class="col-8" style="height: 80vh;">
+    <img :src="mySrc"> 
       <div class ="q-mx-md text-h4 text-bold"> <div class="q-my-md text-h2 text-bold"><span class="text-purple-9">Privacy</span><span class="text-cyan-7">Wallet</span></div>
       Trading personal dataaaaaaaaaaaaaaaaaaaaaa without leaking privacy.
       </div>
@@ -30,6 +31,9 @@
 export default {
   name: 'PageIndex',
   data() {
+   {
+      mySrc: "../public/index.png"
+  },
     return {
       slide: 'style',
       intro: {
