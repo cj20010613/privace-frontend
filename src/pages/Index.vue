8392c9db84b362c873f998af91a36e8c6dc8cc4a
cj20010613 <68772369+cj20010613@users.noreply.github.com>
2021-05-14 10:49:16 +0800

    9
    1

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,12 +1,13 @@
 <template>
   <q-page class="flex flex-center row">
     <div class="col-12" style="height: 20vh;">
-      <div class="bg"><img :src="mySrc"></div>
+      
     </div>
      
     <div class="col-8" style="height: 80vh;">
    
       <div class ="q-mx-md text-h4 text-bold"> <div class="q-my-md text-h2 text-bold"><span class="text-purple-9">Privacy</span><span class="text-cyan-7">Wallet</span></div>
+      <div class="bg"><img :src="mySrc"></div>
       Trading personal dataaaaaaaaaaaaaaaaaaaaaa without leaking privacy.
       </div>
       <div class="q-ma-md text-h6">
